6b32afcaa433eec93ba3435e23e9925071afcde1
Duzie Uche-Abba <72194611+duzzisantos@users.noreply.github.com>
2022-08-10 13:16:36 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,86 @@
+// Create an object to store the special order data
+
+const specialOrder = {
+    paper: {
+        isAvailable: true,
+        price: 95.99
+    },
+    carton: {
+        isAvailable: true,
+        price: 125.99
+    },
+    adhesiveTape: {
+        isAvailable: true,
+        price: 75.99
+    },
+}
+
+// Make promises for the availability of these three products each
+
+  // Papers
+let papers = () => {
+    if(specialOrder.paper.isAvailable){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(specialOrder.paper)
+                console.log(`Paper is available at $${specialOrder.paper.price}`)
+            }, 2000)
+        })
+    } else {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(specialOrder.paper)
+                console.log(`Paper is unavailable at the moment`)
+            }, 2000)
+        })
+    }
+}
+
+ //Cartons
+let cartons = () => {
+    if(specialOrder.carton.isAvailable){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(specialOrder.carton)
+                console.log(`Carton is available at $${specialOrder.carton.price}`)
+            }, 2000)
+        })
+    } else {
+         return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(specialOrder.carton)
+                console.log(`Carton is unavailable at the moment`)
+            }, 2000)
+        })
+    }
+}
+
+ //Adhesive tapes
+let adhesiveTapes = () => {
+     if(specialOrder.adhesiveTape.isAvailable){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(specialOrder.adhesiveTape)
+                console.log(`Adhesive tape is available at $${specialOrder.adhesiveTape.price}`)
+            }, 2000)
+        })
+    } else {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(specialOrder.carton)
+                console.log(`Adhesive tape is unavailable at the moment`)
+            }, 2000)
+        })
+    }
+}
+
+// A function that validates the availability of these three products for special order
+// If any of them is available, the system notifies you, and the reason for the promise rejection is provided
+
+let validateSpecialOrder = () => {
+    Promise.all([papers(), cartons(), adhesiveTapes()]).then((values) => {
+        console.log(values)
+    })
+}
+
+// Side note: You may tweak the data, to see the various outcomes
